2a47a33d9c8c3ffba6c8d2873c1d6c45f660ac8b
Charles Baker <bakercharles14@gmail.com>
2020-05-26 15:14:05 -0400

    added blog component

---
+++ client/src/components/Blog.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Blog extends Component {
+    state = {
+        title: '',
+        content: '',
+        image: ''
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}

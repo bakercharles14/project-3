2a4fb7ff3424b4929533679c6899f7a1c7668df4
Charles Baker <bakercharles14@gmail.com>
2020-05-26 10:37:35 -0400

    added basic page layout to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import HomePage from './components/HomePage.js'
+import Products from './components/Products.js'
+import SingleProduct from './components/SingleProduct.js'
+import Cart from './components/Cart.js'
 import './App.css';
 
 export default class App extends React.Component {
@@ -8,8 +11,64 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <Router>
+          <nav className='top-nav-bar'>
+            <Link to='/'>
+              <div className='home-icon'>
+                <img src="/homebutton.jpg" height="50" width="60" alt="home button"></img>
+              </div>
+            </Link>
+            <Link to='/cart' className='cart-icon'>
+              <span className="material-icons">
+                local_grocery_store
+              </span>
+              <span className='items-in-cart'>
+                0
+              </span>
+            </Link>
+          </nav>
+          <div className='splash-image'>
+            <h1 className='splash-text'>Locally Sourced.</h1>
+            <Link to='/'>
+              <img className='logo' src='/honey-logo.png' alt='honey logo' height='200' width='240' />
+            </Link>
+            <h1 className='splash-text'>Made With Love.</h1>
+          </div>
+          <nav className='center-nav-bar'>
+            <Link to='/products' className='nav-links'>
+              <div className='products-nav-icon'>
+                <span className="material-icons">
+                  store_mall_directory
+            </span> <h3 className='nav-words'>Products</h3>
+              </div>
+            </Link>
+            <Link to='/contact' className='nav-links'>
+              <div className='contact-nav-icon'>
+                <span className="material-icons">
+                  contacts
+            </span> <h3 className='nav-words'>Contact</h3>
+              </div>
+            </Link>
+            <Link to='/blog' className='nav-links'>
+              <div className='blog-nav-icon'>
+                <span className="material-icons">
+                  insert_comment
+            </span> <h3 className='nav-words'>Blog</h3>
+              </div>
+            </Link>
+            <Link to='/about' className='nav-links'>
+              <div className='about-nav-icon'>
+                <span className="material-icons">
+                  menu_book
+            </span> <h3 className='nav-words'>About</h3>
+              </div>
+            </Link>
+          </nav>
+
           <Switch>
             <Route exact path="/" component={HomePage} />
+            <Route exact path="/products" component={Products} />
+            <Route exact path="/products/:productId" component={SingleProduct} />
+            <Route exact path="/cart" component={Cart} />
           </Switch>
         </Router>
       </div>
--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
 
-    / app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(`${__dirname}/client/public`))
 
 
 app.use('/api/product', productRouter)

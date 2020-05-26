3ae8d4b7e9dba2586f2fcaf64ca3ad5ccbe4c3b3
Charles Baker <bakercharles14@gmail.com>
2020-05-26 15:14:35 -0400

    Fixed cart and product components

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import Products from './Products.js'
+import { cart, removeItemFromCart } from '../CartData.js'
 
 export default class Cart extends Component {
+
+    state = {
+        products: [],
+        totalPrice: 0,
+        quantity: 0
+    }
+
+    componentDidMount() {
+        this.setState(cart)
+    }
+
     render() {
         return (
             <div>
+                <h1>Items in your Cart</h1>
+                {cart.products.map((product, index) => {
+                    return (<div key={`kjdhfslksdfsdh-${index}`}>
+                        <div>{product.name}</div>
+                        <div>{product.description}</div>
+                        <div>{product.price}</div>
+                        <input onClick={removeItemFromCart} type='submit' value='Remove Item From Cart' />
+                    </div>)
+                })}
 
             </div>
         )
--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import ProductData from '../ProductData.js'
+import { cart } from '../CartData.js'
 import { Link } from 'react-router-dom'
 
 export default class Products extends React.Component {
+    state = {
+        products: [],
+        totalPrice: 0,
+        quantity: 0
+    }
+
+    componentDidMount() {
+        this.setState(cart)
+    }
+
+
+    // addToCart = async (evt, product) => {
+    //     evt.preventDefault()
+    //     try {
+    //         const newState = await { ...this.state }
+    //         newState.cart.products.push(product.data)
+    //         this.setState(newState)
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+
     render() {
         return (
             <div>
@@ -10,7 +33,7 @@ export default class Products extends React.Component {
                 {ProductData.map((product, index) => {
                     return (
                         <div key={`;sdl;kfhsa;dlkh-${index}`} className='product-item'>
-                            <Link to={`/product/${product._id}`}>
+                            <Link to={`/products/${index}`}>
                                 <img className='product-image' src={product.image} alt={product.name} width='270' height='350' />
                             </Link>
                             <div className='product-words'>
@@ -18,6 +41,12 @@ export default class Products extends React.Component {
                                 <div><strong>Price: </strong>{product.price}</div>
                                 <div className='product-description'><strong>Description: </strong>{product.description}</div>
                             </div>
+                            <input onClick={() => {
+                                const newState = { ...this.state }
+                                newState.products.push(product)
+                                this.setState(newState)
+                                console.log(this.state.products)
+                            }} type='submit' value='Add to Cart' />
                         </div>
                     )
                 })}
